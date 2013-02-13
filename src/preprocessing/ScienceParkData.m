2c072b1ca6ce0d235ea12456a9801d9bcb270566
Ninghang Hu <n.hu@uva.nl>
2013-02-13 16:08:21 +0100

    added methods to select subset of data

--- src/preprocessing/ScienceParkData.m
+++ src/preprocessing/ScienceParkData.m
@@ -1,17 +1,20 @@
-classdef ScienceParkData
+classdef ScienceParkData < handle
   % SCIENCEPARKDATA load the data collected with Kinect and two fisheye
   % cameras
   
   properties
-    Now = 1; % current data item being processed
-    Points % skeleton points
+    Now = 1;  % current data item being processed
+    Points    % skeleton points
     Imagelist % list of imagefiles
     Timestamp % timestamp
     CamFolder %
     VidFolderIdx % video index in the camera folder
+    ActionID    % action labels
   end
   
   properties (Constant)
+    ActionLabels = {...
+      'standing' 'bending' 'sitting' 'pointing' 'waving' 'drinking' 'stretching' 'walking'}
     SkeletonLabels = {...
       'head_1' 'neck_1' 'torso_1' ...
       'left_shoulder_1' 'left_elbow_1' 'left_hand_1' ...
@@ -22,6 +25,7 @@ classdef ScienceParkData
   
   methods
     
+    % constructor
     function spd = ScienceParkData(camFolder,vidFolderIdx)
       
       spd.CamFolder = camFolder;
@@ -58,17 +62,43 @@ classdef ScienceParkData
         spd.Imagelist{i} = fullfile(vidFolder,imlist(iidx(i)).name);
       end
       
+      % init action labels as zero matrix
+      spd.ActionID = zeros(length(spd.Imagelist),length(spd.ActionLabels));
+      
     end
-%     
-%     function initPerElement(now,points,imagelist,timestamp,camFolder,vidFolderIdx)
-%       spd.Now = now;
-%       spd.Points = points;
-%       spd.Imagelist = imagelist;
-%       spd.Timestamp = timestamp;
-%       spd.CamFolder = camFolder;
-%       spd.VidFolderIdx = vidFolderIdx;
-%     end
     
+    function append(spd,A)
+      
+      spd.Points = cat(1,spd.Points,A.Points);
+      spd.Imagelist = cat(1,spd.Imagelist,A.Imagelist);
+      spd.Timestamp = cat(1,spd.Timestamp,A.Timestamp);
+      spd.ActionID = cat(1,spd.ActionID,A.ActionID);
+      
+    end
+    
+    % select subset by action label
+    function B = selectLabel(spd,label)
+      
+      c = strcmp(label,spd.ActionLabels);
+      if c == 0
+        disp(spd.ActionLabels);
+        error('incorrect action label, recheck.');
+      else
+        dataIdx = (spd.ActionID(:,c) == 1);
+        B = spd.selectRow(dataIdx);
+      end
+      
+    end
+    
+    % select subset by row index
+    function B = selectRow(spd,idx)
+      B = spd;
+      B.Points = spd.Points(idx,:,:);
+      B.Imagelist = spd.Imagelist(idx);
+      B.Timestamp = spd.Timestamp(idx);
+      B.ActionID = spd.ActionID(idx,:);
+      
+    end
   end
   
 end
